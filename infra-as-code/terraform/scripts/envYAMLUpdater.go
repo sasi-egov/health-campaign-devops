4b3bca2493bce2e8a94a46bd51d29e07b4f2d131
Sreejith Kanjarla <54531041+Sreejit-K@users.noreply.github.com>
2025-02-03 17:42:52 +0530

    Update envYAMLUpdater.go

--- infra-as-code/terraform/scripts/envYAMLUpdater.go
+++ infra-as-code/terraform/scripts/envYAMLUpdater.go
@@ -36,8 +36,9 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error reading YAML file: %v\n", err)
 		os.Exit(1)
 	}
-	output = strings.ReplaceAll(yamlFile, "<db_host_name>", tfOutput.DBHost.Value)
-	output = strings.ReplaceAll(yamlFile, "<db_name>", tfOutput.DBName.Value)
+	output := string(yamlFile)
+	output = strings.ReplaceAll(output, "<db_host_name>", tfOutput.DBHost.Value)
+	output = strings.ReplaceAll(output, "<db_name>", tfOutput.DBName.Value)
 
 	// Write the updated YAML to stdout
 	fmt.Println(output)
